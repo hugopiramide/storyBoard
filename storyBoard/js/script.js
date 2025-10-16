5d092e85a8aa032eb9113532585821adf14932d4
hugopiramide <hugocastillo.deus@gmail.com>
2025-10-16 08:56:06 +0200

    Update issues

--- storyBoard/js/script.js
+++ storyBoard/js/script.js
@@ -1,4 +1,3 @@
-// Menú responsive
 document.getElementById('hamburger').addEventListener('click', function() {
   const navMenu = document.querySelector('.nav-menu');
   navMenu.classList.toggle('show');
